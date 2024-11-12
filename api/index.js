fcbb8a98f4045639c6ac884775d4bbad79b19249
devAlen01 <alenermekuulu@gmail.com>
2024-11-12 15:47:30 +0600

    ...

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 5001; // Измените порт на 5001
+const PORT = process.env.PORT || 5001;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

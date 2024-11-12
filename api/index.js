778ac0f7189a10376a1a7310bd278a3d6b8dcdd0
devAlen01 <alenermekuulu@gmail.com>
2024-11-12 16:39:52 +0600

    ....

--- api/index.js
+++ api/index.js
@@ -1,12 +1,27 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocumentation from "../swagger.json" assert { type: "json" };
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+
+// Получаем путь к текущему файлу и директории
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Подключение Swagger UI
+const swaggerDocumentation = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "..", "swagger.json"), "utf8")
+);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -15,7 +30,6 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Сервер работает" });
 });
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
 
 app.get("/all-posts", async (req, res) => {
   const allPost = await prisma.post.findMany();
@@ -24,16 +38,13 @@ app.get("/all-posts", async (req, res) => {
 
 app.get("/one-post/:id", async (req, res) => {
   const { id } = req.params;
-  const allPost = await prisma.post.findMany();
-  const findIndex = allPost.findIndex((el) => el.id === +id);
-
-  if (findIndex === -1) {
-    return res.status(400).json({ error: "Нет такого ID" });
-  }
-
   const onePost = await prisma.post.findUnique({
     where: { id: Number(id) },
   });
+
+  if (!onePost) {
+    return res.status(400).json({ error: "Нет такого ID" });
+  }
 
   res.status(200).json(onePost);
 });
@@ -56,12 +67,14 @@ app.post("/create", async (req, res) => {
 
 app.delete("/delete/:id", async (req, res) => {
   const { id } = req.params;
-  const allPost = await prisma.post.findMany();
-  const findIndex = allPost.findIndex((el) => el.id === +id);
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
 
-  if (findIndex === -1) {
+  if (!post) {
     return res.status(400).json({ error: "Нет такого ID" });
   }
+
   await prisma.post.delete({
     where: { id: Number(id) },
   });
@@ -71,13 +84,16 @@ app.delete("/delete/:id", async (req, res) => {
 app.put("/update/:id", async (req, res) => {
   const { id } = req.params;
   const { title, content, imageUrl } = req.body;
-  const allPost = await prisma.post.findMany();
-  const findIndex = allPost.findIndex((el) => el.id === +id);
-  if (findIndex === -1) {
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!post) {
     return res.status(400).json({ error: "Нет такого ID" });
   } else if (!title || !content) {
     return res.status(404).json({ message: "Ошибка при добавлении" });
   }
+
   const updatedPost = {
     title,
     content,
